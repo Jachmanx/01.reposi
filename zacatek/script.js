ea6ecf9d44c1c381a0c4a805ff1a95f863a75520
Jachmanx <145440437+Jachmanx@users.noreply.github.com>
2023-10-23 12:22:07 +0200

    java

---
+++ zacatek/script.js
@@ -0,0 +1,24 @@
+function getName() {
+  const iUserName = document.getElementById("username").value;
+    
+  console.log(inputUserName)
+  }
+
+  switch(operator) {
+    case '+':
+      result = firstNumber + secondNumber;
+      break;
+    case '-':
+      result = firstNumber - secondNumber;
+      break;
+    case '*':
+      result = firstNumber * secondNumber;
+      break;
+    case '/':
+      result = firstNumber / secondNumber;
+      break;
+  }
+
+  function checkResult() {
+    const inputNumber = parseInt(document.getEle
+  }
