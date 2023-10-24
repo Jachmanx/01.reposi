60c8236087fd174c6e573b7b84d85b10fd8110b3
Jachmanx <145440437+Jachmanx@users.noreply.github.com>
2023-10-24 13:12:59 +0200

    java zmatek

---
+++ zacatek/02.java/script.js
@@ -0,0 +1,16 @@
+function pytagorova () {
+    const inputFirstNumber = parseInt(document.getElementById("firstNumber").value)
+    const inputSecondNumber = parseInt(document.getElementById("secondNumber").value)
+
+    document.getElementById("result").value = Math.sqrt(additionResult)
+}
+
+    function ifFuncion() {
+        if (a > 0)
+        const inputFirstNumber = document.getElementById("firstNumber")
+        if (inputFirstNumber > 0) {
+        }
+        else if (a > 10)
+        console.log(>10)
+        
+    }
