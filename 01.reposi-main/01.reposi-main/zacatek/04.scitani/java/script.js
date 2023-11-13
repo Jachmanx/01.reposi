f01827bfaf1fcc8353f89b23050c876747814937
Jachmanx <145440437+Jachmanx@users.noreply.github.com>
2023-11-13 11:12:05 +0100

    Add files via upload

---
+++ 01.reposi-main/01.reposi-main/zacatek/04.scitani/java/script.js
@@ -0,0 +1,17 @@
+
+        function sumNumbers(){
+            let inputNumber = parseInt(document.getElementById("inputNumber").value)
+
+            outputNumber = 0
+
+            if (number % 2 == 0) {
+                ystem.out.println(number + " is even.");
+               
+            }
+            document.getElementById("outputNumber").innerHTML = outputNumber
+        }
+
+        
+       // if (number % 2 == 0) {
+          //  System.out.println(number + " is even.");
+          //outputNumber = outputNumber + i
